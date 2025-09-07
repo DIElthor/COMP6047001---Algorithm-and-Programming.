ede69e60c05dee4648e90e3002a43dfb11e6984d
DIElthor <darell12345z@gmail.com>
2025-09-07 13:55:21 +0700

    assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,15 @@
-'print("hello")' 
+int main()
+{
+  int area,length,width,area2,length2,width2;
+  length = 3;
+  width = 5;
+  area = length * width;
+  printf ("First assignment : %d\n",area);
+  printf ("Second assignment : \nAdd length ");
+  scanf("%d",&length2);
+  printf ("Add width ");
+  scanf("%d",&width2);
+  area2 = length2 * width2;
+  printf ("%d",area2);
+  return 0;
+}
